eb2311bfd8ebd3210b04ec009a0b759640b4bdf9
Penuel Maypa (x16382003) <2023.nci.pen@gmail.com>
2023-12-06 20:38:56 +0000

    Amending the scaffolding 	- Command 	------------------------ 		rails generate model DishesIngredient dish:references ingredient:references 	---------------------------

--- dish_recipe_planner/app/models/dish.rb
+++ dish_recipe_planner/app/models/dish.rb
@@ -1,2 +1,4 @@
 class Dish < ApplicationRecord
+    has_many :dishes_ingredients
+    has_many :ingredients, through: :dishes_ingredients
 end
---
+++ dish_recipe_planner/test/models/dishes_ingredient_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DishesIngredientTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
