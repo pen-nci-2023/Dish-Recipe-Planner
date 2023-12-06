6c045fccbaeb7fe1c6d75aea4de4238c6f604828
Penuel Maypa (x16382003) <2023.nci.pen@gmail.com>
2023-12-06 20:09:38 +0000

    I've set up a scaffolding , Ingredient and Dish: 	Commands 	--------------------------------
    		rails generate scaffold Ingredient name:string quantity:integer
    
    		rails generate scaffold Dish name:string ingredient_id:integer
    
    	---------------------------------

--- dish_recipe_planner/config/routes.rb
+++ dish_recipe_planner/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :dishes
+  resources :ingredients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
